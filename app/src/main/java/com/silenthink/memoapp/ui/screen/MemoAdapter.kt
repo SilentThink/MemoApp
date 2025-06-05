08ea2f5419407531d7fbccee88a9b2986d9b5443
Your Name <you@example.com>
2025-06-05 20:35:39 +0800

    🐛 fix: 修复优先级为普通时，”普通“文字显示不出来的问题

--- app/src/main/java/com/silenthink/memoapp/ui/screen/MemoAdapter.kt
+++ app/src/main/java/com/silenthink/memoapp/ui/screen/MemoAdapter.kt
@@ -4,10 +4,12 @@ import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.silenthink.memoapp.R
 import com.silenthink.memoapp.data.model.Memo
 import com.silenthink.memoapp.databinding.ItemMemoBinding
 import com.silenthink.memoapp.util.CategoryUtils
@@ -56,7 +58,15 @@ class MemoAdapter(
             
             // 显示优先级
             binding.tvPriority.text = CategoryUtils.Priority.getPriorityText(memo.priority)
-            binding.tvPriority.setTextColor(Color.parseColor(CategoryUtils.Priority.getPriorityColor(memo.priority)))
+            binding.tvPriority.setTextColor(Color.WHITE)
+            
+            // 根据优先级设置不同的背景
+            val backgroundResource = when (memo.priority) {
+                CategoryUtils.Priority.IMPORTANT -> R.drawable.priority_important_background
+                CategoryUtils.Priority.URGENT -> R.drawable.priority_urgent_background
+                else -> R.drawable.priority_normal_background
+            }
+            binding.tvPriority.background = ContextCompat.getDrawable(binding.root.context, backgroundResource)
             binding.tvPriority.visibility = View.VISIBLE
             
             // 显示图片缩略图
